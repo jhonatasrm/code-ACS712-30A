5f2f735bbba6ac7c81cfb9f195666a59f65dd1e5
Jhonatas Rodrigues <jhonatasrm@gmail.com>
2019-08-15 01:08:47 -0300

    Added code + .gitignore

---
+++ code_ACS712_30A.ino
@@ -0,0 +1,44 @@
+float vetCorrente[300];
+
+void setup(){
+    Serial.begin(9600); pinMode(A0, INPUT);
+}
+
+void loop(){
+    double maior_Valor = 0;
+    double valor_Corrente = 0;
+    float tensao = 127;
+    float potencia = 0;
+
+for(int i = 0; i < 300; i++) {
+    vetCorrente[i] = analogRead(A0);
+    delayMicroseconds(600);
+}
+
+for(int i = 0; i < 300; i++) {
+    if(maior_Valor < vetCorrente[i]) {
+        maior_Valor = vetCorrente[i];
+    }
+}
+
+maior_Valor = maior_Valor * 0.004882812;
+valor_Corrente = maior_Valor - 2.5;
+valor_Corrente = valor_Corrente * 1000;
+valor_Corrente = valor_Corrente / 66;
+valor_Corrente = valor_Corrente / 1.41421356;
+
+Serial.print("BT High Performance");
+Serial.print("Corrente = ");
+
+Serial.print(valor_Corrente);
+
+Serial.println(" A”);
+potencia = valor_Corrente * tensao;
+
+Serial.print("Potencia = ");
+Serial.print(potencia);
+Serial.println(" W");
+Serial.print(".");
+delay(60000);
+Serial.println("");
+}
